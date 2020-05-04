7341c511c647932be84a73eac359798795e91baa
Maëlys <maelys.marry@insa-lyon.fr>
2020-05-04 17:14:58 +0200

    Initial commit

--- Evolution.h
+++ Evolution.h
@@ -3,9 +3,15 @@
 class Evolution 
 {
 	public : 
+		//Constructor
 		Evolution();
+
+		//Destructor
 		~Evolution();
 
+		// Getters
+
+		//Functions
 		Node[] replication(Node parent, int number_of_child);
 		Node mutation(Node parent);
 		Node * fitness (Node[] tableau_enfants_mutes);
