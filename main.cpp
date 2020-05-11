c110b9cd1db630a157e4e9e96273620dec941b61
DINH Son-Michel <son-michel.dinh@insa-lyon.fr>
2020-05-11 16:30:17 +0200

    Added code to main.cpp

--- main.cpp
+++ main.cpp
@@ -10,27 +10,23 @@ int main(int argc, char const *argv[])
 	std::string operator_and("AND");
 	std::string operator_not("NOT");
 
-	Node gaucgedroite(NULL, NULL, operand_true) // Niveau 3
-	Node gauchegauche(NULL, NULL, operand_false) // Niveau 3
+	Node gaucgedroite(NULL, NULL, operand_true); // Niveau 3
+	Node gauchegauche(NULL, NULL, operand_false); // Niveau 3
+	Node gauche(&gauchegauche, &gaucgedroite, operator_and); // Niveau 2
 
-	Node droitegauche(NULL, NULL, operand_false) // Niveau 3
+	Node droitegauche(NULL, NULL, operand_false); // Niveau 3
+	Node droite(&droitegauche, NULL, operator_not); // Niveau 2
 
-
-	Node gauche(*gauchegauche, *gaucgedroite, operator_and); // Niveau 2
-	
-	Node droite(droitegauche, NULL, operator_not); // Niveau 2
-
-	
 	Node racine(&gauche, &droite, operator_or); // Niveau 1
 
 
-	std::cout << Résultat arbre : << std::endl;
+	std::cout << "Résultat arbre :" << std::endl;
 	std::cout << racine.node_result() << std::endl;
 
-	std::cout << Résultat noeud gauche : << std::endl;
+	std::cout << "Résultat noeud gauche :" << std::endl;
 	std::cout << gauche.node_result() << std::endl;
 
-	std::cout << Résultat noeud droite : << std::endl;
+	std::cout << "Résultat noeud droite :" << std::endl;
 	std::cout << droite.node_result() << std::endl;
 
 
