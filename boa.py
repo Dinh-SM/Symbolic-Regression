7a27602814ceb6d8d1778cbf67feac769fb258f9
maelle <maelle.broustal@insa-lyon.fr>
2020-06-08 14:30:56 +0200

    Seems like it works

--- boa.py
+++ boa.py
@@ -1,11 +1,10 @@
 #Hey
-import subprocess
+import os
 
 cycles = input("Nombre de cycles de calcul: ")
 children = input("Nombre de descendants attendus: ")
 name_file = input("Fichier à exploiter: ")
 
-subprocess.Popen("g++ -o test main.cpp")
+os.system("g++ -o test main.cpp")
 command = "./test " + cycles + " " + children + " " + name_file;
-print(command)
-subprocess.Popen(command)
+os.system(command)
