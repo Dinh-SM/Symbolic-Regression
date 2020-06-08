0971fcb537d7b63d69721be651bbe4b90e7e35cc
Mouchiroud Virgile <virgile.mouchiroud@free.fr>
2020-06-08 15:53:21 +0200

    The algorithm now prints the graph of the evolution of the fitness for each cycle

--- boa.py
+++ boa.py
@@ -1,5 +1,7 @@
-#Hey
+#Hey oh, let's go !
 import os
+import pylab as plt
+import numpy as np
 
 cycles = input("Nombre de cycles de calcul: ")
 children = input("Nombre de descendants attendus: ")
@@ -8,3 +10,12 @@ name_file = input("Fichier à exploiter: ")
 os.system("g++ -o test main.cpp")
 command = "./test " + cycles + " " + children + " " + name_file;
 os.system(command)
+
+x, y = np.loadtxt('fitness_progression.out', delimiter='\t', unpack=True)
+plt.plot(x,y, label='fitness value')
+
+plt.xlabel('Cycle')
+plt.ylabel('Fitness')
+plt.title('Fitness evolution for each cycle')
+plt.legend()
+plt.show()
